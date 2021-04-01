4cb61085b753fd7b4bbcb1c2640fbc07fba01aae
kamruzzamanripon <rkamruzzaman@gmail.com>
2021-04-02 00:30:02 +0600

    again update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Game from './component/game'
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello Nashwan Tik Tac Toe</h1>
+        <Game/>
+    </div>
+  );
+}
+
+export default App;
